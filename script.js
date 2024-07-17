86362879408a293dd509ae1717357450356271c0
PriyankaRC16 <rupali317@gmail.com>
2024-07-17 10:36:17 +0800

    Refactor the javascript code by using id selectors and added aria attributes to enhance the screen reader experience

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const tooltip = document.querySelector(".tooltip-wrapper-js");
-const shareButton = document.querySelector(".share-btn-js");
+const tooltip = document.querySelector("#tooltip-wrapper-js");
+const shareButton = document.querySelector("#share-btn-js");
 
 shareButton.addEventListener("click", () => {
   toggleTooltip();
@@ -11,4 +11,17 @@ tooltip.addEventListener("click", () => {
 
 function toggleTooltip() {
   tooltip.classList.toggle("invisible");
+  toggleShareButtonAttributes();
 }
+
+function toggleShareButtonAttributes() {
+  if (tooltip.classList.contains("invisible")) {
+    shareButton.removeAttribute("aria-describedby");
+    shareButton.setAttribute("aria-expanded", "false");
+  } else {
+    shareButton.setAttribute("aria-describedby", "tooltip");
+    shareButton.setAttribute("aria-expanded", "true");
+  }
+}
+
+/* RRC: Understand the purpose of the attributes by experimenting */
